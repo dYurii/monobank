515f4109a0b5f5d44230a9985a7d1cb705a44f85
PashkaSm <smolynets_pavlo@ukr.net>
2022-06-03 21:32:18 +0300

    arrow style

--- src/components/sidebar/SidebarItem.jsx
+++ src/components/sidebar/SidebarItem.jsx
@@ -5,7 +5,11 @@ function SidebarItem({ sideItem }) {
     <Link to={{ pathname: `${sideItem.link}` }} className="sidebar-item">
       <div className="side-image">{sideItem.logo}</div>
       <div className="side-tag">{sideItem.underLogoText}</div>
-      <div className="side-selected"></div>
+      <div className="arrov">
+        <div className="top"></div>
+        <div className="up"></div>
+      </div>
+      {/* <div className="side-selected"></div> */}
     </Link>
   );
 }
