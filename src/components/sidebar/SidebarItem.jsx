766e0697ecab803670fb3fe469b089acb27f6496
dYurii <dubyk1997@gmail.com>
2022-06-04 13:58:26 +0300

    commit

--- src/components/sidebar/SidebarItem.jsx
+++ src/components/sidebar/SidebarItem.jsx
@@ -5,11 +5,7 @@ function SidebarItem({ sideItem }) {
     <Link to={{ pathname: `${sideItem.link}` }} className="sidebar-item">
       <div className="side-image">{sideItem.logo}</div>
       <div className="side-tag">{sideItem.underLogoText}</div>
-      <div className="arrov">
-        <div className="top"></div>
-        <div className="up"></div>
-      </div>
-      {/* <div className="side-selected"></div> */}
+      <div className="side-selected"></div>
     </Link>
   );
 }
