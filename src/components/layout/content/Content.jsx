65e68fd828105bc75cb43cf1f51e1de3c59df13a
PashkaSm <smolynets_pavlo@ukr.net>
2022-06-08 15:46:31 +0300

    changes grid l

--- src/components/layout/content/Content.jsx
+++ src/components/layout/content/Content.jsx
@@ -5,16 +5,21 @@ import DonutGrap from "../../graphs/DonutGraph";
 import { useState } from "react";
 import TransactionList from "../../transactions/TransactionList";
 function Content() {
+  const changeColor = () => {
+    setUserData(previousState => {
+      return { ...previousState, date: 0 }
+    })
+  };
   const [userData, setUserData] = useState({
-    labels: ['пн', 'вт', 'ср','чт','пт','сб', 'нд'],
+    labels: ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'нд'],
     datasets: [{
       label: 'Test',
-      data: [20 , 33, 3,7,5,100, 33, 34,7,5],
+      data: [20, 33, 3, 7, 5, 100, 33, 34, 7, 5],
       backgroundColor: [
         "rgb(117,83,230)"
       ],
       tension: 0.4,
-      borderWidth : 4,
+      borderWidth: 4,
     }
     ]
   });
@@ -49,21 +54,22 @@ function Content() {
     },
   });
   const [userDataDounut, setUserDataDounut] = useState({
-    labels: ['пн', 'вт', 'ср','чт','пт','сб', 'нд'],
+    labels: ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'нд'],
     datasets: [{
       label: 'Test',
-      data: [20 , 33, 3,7,5,20],
+      data: [20, 33, 3, 7, 5, 20],
       backgroundColor: [
-        "rgb(117,83,230)","rgb(17,100,230)","rgb(17,8,30)",
-        "rgb(117,8,0)","rgb(0,0,230)","rgb(17,83,20)"
+        "rgb(117,83,230)", "rgb(17,100,230)", "rgb(17,8,30)",
+        "rgb(117,8,0)", "rgb(0,0,230)", "rgb(17,83,20)"
       ],
-      tension: 0.4
     }
     ]
   });
+
+  const [color, setColor] = useState("red");
   const [userOptionDounut, setUserOptionDounut] = useState({
-    responsive: false,
-    maintainAspectRatio: false,
+    responsive: true,
+    maintainAspectRatio: true,
     plugins: {
       title: {
         display: false,
@@ -98,17 +104,18 @@ function Content() {
         <CardItem />
         <CardItem />
       </div>
-      <div className="grap">
+      <div className="money-wrapper">
         <div className="linear-wrapper">
           <LinearGraph charDate={userData} option={userOption} />
         </div>
         <div className="donut-wrapper">
           <DonutGrap charDate={userDataDounut} option={userOptionDounut} />
         </div>
+        <div className="transaction-wrapper">
+          <TransactionList />
+        </div>
       </div>
-      <div className="transaction-wrapper">
-        <TransactionList />
-      </div>
+      <button onClick={changeColor}>{color}</button>
     </div>
   );
 }
